5681564e086f1d083cfec18576eebd5f66ce9a5e
Dana Ward <daward@vistaprint.com>
2017-12-14 12:55:00 -0500

    making our first component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Jamz</h1>
-        </header>
+        <Welcome message="Welcome to Jamz"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
